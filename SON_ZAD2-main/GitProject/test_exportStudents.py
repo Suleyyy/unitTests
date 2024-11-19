0808a4aac403a7055479778c4167dc98fdbe1089
MikolajSulkowski <mikolajsulkowski675@gmail.com>
2024-11-19 17:43:26 +0100

    feat: export test works omg

--- SON_ZAD2-main/GitProject/test_exportStudents.py
+++ SON_ZAD2-main/GitProject/test_exportStudents.py
@@ -1,17 +1,36 @@
 from exportStudents import ExportStudents
+import os
 
-class testExportStudents:
-    mock = ExportStudents.csv
-    mock2 = ExportStudents.txt
+class TestExportStudents:
 
-    def test_csv(self):
+    @staticmethod
+    def test_csv():
         #Given
+        mock = ExportStudents.csv
         path = 'C:/Users/mikol/Desktop/unitTests/SON_ZAD2-main/GitProject/lists/temp.csv'
         students = [{'Name': 'John', 'Surname': 'Snow'}, {'Name': 'Janusz', 'Surname': 'Tracz'}]
-        self.mock(path, students)
-        want = ['John;Snow\nJanusz;Tracz']
+        mock(path, students)
+        want = 'John;Snow\nJanusz;Tracz'
         file = open(path, 'r')
         #When
         got = file.read()
         #Then
         assert want == got
+        file.close()
+        os.remove(path)
+
+    @staticmethod
+    def test_txt():
+        #Given
+        mock = ExportStudents.txt
+        path = 'C:/Users/mikol/Desktop/unitTests/SON_ZAD2-main/GitProject/lists/temp.txt'
+        students = [{'Name': 'John', 'Surname': 'Snow'}, {'Name': 'Janusz', 'Surname': 'Tracz'}]
+        mock(path, students)
+        want = 'John Snow\nJanusz Tracz'
+        file = open(path, 'r')
+        #When
+        got = file.read()
+        #Then
+        assert want == got
+        file.close()
+        os.remove(path)
